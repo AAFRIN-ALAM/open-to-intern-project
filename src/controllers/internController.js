cf9e28ba953be1fdff90248cfad095cde235938c
AAFRIN-ALAM <98942359+AAFRIN-ALAM@users.noreply.github.com>
2022-03-23 23:10:00 +0500

    Update internController.js

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,6 +1,7 @@
 const internModel = require("../models/internModel");
 const collegeModel = require("../models/collegeModel");
 
+// Validation function
 const isValid = function (value) {
   if (typeof value == undefined || value == null) return false;
   if (typeof value === "string" && value.trim().length === 0) return false;
@@ -10,57 +11,72 @@ const isValid = function (value) {
 const createIntern = async function (req, res) {
   try {
     const data = req.body;
-    const { name, email, mobile } = data;
+    const { name, email, mobile,collegeId } = data;
 
     if (Object.keys(data) == 0) {
-      return res.status(400).send({ status: false, msg: " data is  missing" });
+      return res.status(400).send({ status: false, msg: " Data is  missing" });
     }
+
     const request = isValid(name);
     if (!request) {
-      return res.status(400).send({ status: false, msg: " name is required" });
+      return res.status(400).send({ status: false, msg: " Name is required" });
     }
+
     const request1 = isValid(email);
     if (!request1) {
-      return res.status(400).send({ status: false, msg: " email is required" });
+      return res.status(400).send({ status: false, msg: " Email is required" });
     }
 
     const request2 = isValid(mobile);
     if (!request2) {
       return res
         .status(400)
-        .send({ status: false, msg: " mobile is required" });
+        .send({ status: false, msg: " Mobile is required" });
     }
-    const isEmailAlreadyUsed = await internModel.findOne({ email });
 
+    const request3 = isValid(collegeId);
+    if (!request3) {
+      return res.status(400).send({ status: false, msg: " Id is required" });
+    }
+    
+    const isEmailAlreadyUsed = await internModel.findOne({ email });
     if (isEmailAlreadyUsed) {
       return res
         .status(400)
         .send({ status: false, msg: `${email} email is already used` });
     }
 
+    //Email validating
+    if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email))    
+      return res.status(400).send({ status: false, msg: " Email is invalid" });
+
+
     const isMobileAlreadyUsed = await internModel.findOne({ mobile });
-
-    if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email))
-      return res.status(400).send({ status: false, msg: " email is invalid" });
-
     if (isMobileAlreadyUsed) {
       return res
         .status(400)
         .send({ status: false, msg: `${mobile} mobile is already used` });
     }
 
+    //Mobile validating
     if (!/^(\+\d{1,3}[- ]?)?\d{10}$/.test(mobile))
-      return res.status(400).send({ status: false, msg: " mobile is invalid" });
+      return res.status(400).send({ status: false, msg: " Mobile is invalid" });
+      
+    const Id = await collegeModel.findOne({_id: collegeId})
+    if(!Id)
+            return res.status(404).send({status: false, message: 'Id not found'})
+            const internData = { name, email, mobile, collegeId}
 
-    const saveData = await internModel.create(data);
+    const saveData = await internModel.create(internData);
     res
       .status(201)
       .send({ status: "successful-response-structure", msg: saveData });
-  } catch (err) {
-    res.status(500).send({ error: err.message });
+  } catch (error) {
+    res.status(500).send({ status:false, error: error.message });
   }
 };
 
+//getCollegeDetails
 
 const getCollegeDetails = async (req, res) => {
   try {
@@ -83,7 +99,6 @@ const getCollegeDetails = async (req, res) => {
         .send({ status: false, message: "name Is Required" });
     }
 
-    // collegeName must be a single word
     if (name1.split(" ").length > 1) {
       return res
         .status(400)
@@ -93,7 +108,6 @@ const getCollegeDetails = async (req, res) => {
         });
     }
 
-    // if name is invalid
     const collegeNames = await collegeModel.findOne({ name: name1 });
     if (!collegeNames) {
       return res
@@ -112,7 +126,6 @@ const getCollegeDetails = async (req, res) => {
 
     const { name, fullName, logoLink } = collegeNames;
 
-    // Final list of College details with students name who applied for internship
 
     const finalData = {
       name: name,
