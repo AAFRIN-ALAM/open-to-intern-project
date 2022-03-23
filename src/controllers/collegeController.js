390d5f049cf80fa6ef691c92b33b9a1fa095f96f
AAFRIN-ALAM <98942359+AAFRIN-ALAM@users.noreply.github.com>
2022-03-23 23:11:08 +0500

    Update collegeController.js

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,15 +1,16 @@
 const collegeModel = require("../models/collegeModel");
 
+//Validation function
 const isValid = function (value) {
   if (typeof value == undefined || value == null) return false;
   if (typeof value === "string" && value.trim().length === 0) return false;
   return true;
 };
 
-const createCollege = async (req, res) => {
+const createCollege = async (req, res) => {    
   try {
-    let data = req.body;
-    const { name, fullName, logoLink } = data;
+    let data = req.body
+     const { name, fullName, logoLink } = data;
 
     if (Object.keys(data) == 0) {
       return res.status(400).send({ status: false, msg: " Data is  missing" });
@@ -20,16 +21,14 @@ const createCollege = async (req, res) => {
       return res.status(400).send({ status: false, msg: " Name is required" });
     }
 
-    // Abbrevation must be a single word
+    // Abbrevation must be in single word
     const collegeval = name.split(" ");
     const len = collegeval.length;
     if (len > 1) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          msg: "Abbreviated college name should be in a single word",
-        });
+      return res.status(400).send({
+        status: false,
+        msg: "Abbreviated college name should be in single word",
+      });
     }
 
     const input2 = isValid(fullName);
@@ -53,22 +52,28 @@ const createCollege = async (req, res) => {
         .send({ status: false, msg: "Name  is already used" });
     }
 
-    const isFulllreadyUsed = await collegeModel.findOne({ fullName });
-    if (isFulllreadyUsed) {
+    const isFullNameAlreadyUsed = await collegeModel.findOne({ fullName });
+    if (isFullNameAlreadyUsed) {
       return res
         .status(400)
         .send({ status: false, msg: "Fullname is already used" });
     }
 
-    const isLogolinkreadyUsed = await collegeModel.findOne({ logoLink });
-    if (isLogolinkreadyUsed) {
+    const isLogoLinkAlreadyUsed = await collegeModel.findOne({ logoLink });
+    if (isLogoLinkAlreadyUsed) {
       return res
         .status(400)
         .send({ status: false, msg: "Logolink is already used" });
     }
 
-    let saveData = await collegeModel.create(data);
-    res.status(201).send({ status: true, msg: saveData });
+    // To validate logoLink
+    if (!/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(logoLink))
+      return res
+        .status(400)
+        .send({ status: false, msg: " logoLink is invalid" });
+
+    let saveData = await collegeModel.create(data);    
+    res.status(201).send({ status: true, msg: saveData });  
   } catch (error) {
     res.status(500).send({ status: false, msg: error.message });
   }
