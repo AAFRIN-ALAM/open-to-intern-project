4b900d4ab2bfcdde3b6b9cf543ec34a6b2245062
AAFRIN-ALAM <98942359+AAFRIN-ALAM@users.noreply.github.com>
2022-05-15 19:31:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://Aafrin77:omaJBV2vPYhwOS7f@cluster0.ekfff.mongodb.net/groupXDatabase", {
+mongoose.connect("mongodb+srv://Aafrin77:omaJBV2vPYhwOS7f@cluster0.ekfff.mongodb.net/Aafrin2", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
